937cedb349876c719cb284a1c0c185288e60376c
Tushar Agrawal <tusharagrawal16@rediffmail.com>
2021-04-25 21:23:36 +0530

    Geting into it

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I just edited this app</Text>
       <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="enter a task" />
+        <Button title="Add task" />
+      </View>
+      <View><Text>This iis s a text</Text></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex:1,
+    backgroundColor: "#adf",
+    paddingVertical: 40,
+    paddingHorizontal: 10,
+    flexDirection:"column",
+    // alignItems: "flex-start"
+    
+  },
+  inputContainer: {
+    flexDirection: "row",
+    backgroundColor: "#fd34",
+    justifyContent: "space-between",
   },
 });
